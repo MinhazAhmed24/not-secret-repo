cc4ddca700eeb21a10fcb57b18d2ea03351aea8e
David Haynes <dhaynes@cloudflare.com>
2024-02-20 13:27:16 -0800

    Add title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world - this is the landing page for the <code>not-secret-repo</code> project! 
         </p>
         <a
           className="App-link"
